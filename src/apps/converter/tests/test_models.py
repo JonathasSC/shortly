851ce9d6fa3277f0602b9df3b4c8dd972e6c87f4
JonathasSC <jonathas.s.cardoso28@gmail.com>
2025-10-04 21:49:53 -0300

    test(models): fix Url and AccessEvent model tests

--- src/apps/converter/tests/test_models.py
+++ src/apps/converter/tests/test_models.py
@@ -1,26 +1,30 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.utils import timezone
 
 from apps.converter.models import AccessEvent, Url
+
+User = get_user_model()
 
 
 class UrlModelTestCase(TestCase):
     def setUp(self):
-        User.objects.create_user(
+        self.user = User.objects.create_user(
             username='Example',
             email='example@email.com'
         )
 
-        user = User.objects.get(email='example@email.com')
-
-        Url.objects.create(
+        self.url = Url.objects.create(
             original_url='https://sh0rtly.com',
-            created_by=user
+            created_by=self.user
         )
 
     def test_url_create(self):
         url = Url.objects.get(original_url='https://sh0rtly.com')
         self.assertEqual(url.original_url, 'https://sh0rtly.com')
+        self.assertIsNotNone(url.short_code)
+        self.assertIsNotNone(url.expires_at)
+        self.assertFalse(url.is_expired())
 
 
 class AccessEventModelTestCase(TestCase):
@@ -38,28 +42,26 @@ class AccessEventModelTestCase(TestCase):
     def test_access_event_creation(self):
         event = AccessEvent.objects.create(
             url=self.url,
-            ip_address='123.123.123.123',
-            counter=1
+            ip_address='123.123.123.123'
         )
 
         self.assertIsNotNone(event.pk)
         self.assertEqual(event.url, self.url)
         self.assertEqual(event.ip_address, '123.123.123.123')
-        self.assertEqual(event.counter, 1)
+        self.assertIsNotNone(event.created_at)
 
     def test_multiple_access_events(self):
         AccessEvent.objects.create(
             url=self.url,
-            ip_address='111.111.111.111',
-            counter=1
+            ip_address='111.111.111.111'
         )
         AccessEvent.objects.create(
             url=self.url,
-            ip_address='222.222.222.222',
-            counter=2
+            ip_address='222.222.222.222'
         )
 
-        events = AccessEvent.objects.filter(url=self.url)
+        events = AccessEvent.objects.filter(
+            url=self.url).order_by('created_at')
         self.assertEqual(events.count(), 2)
         self.assertEqual(events[0].ip_address, '111.111.111.111')
         self.assertEqual(events[1].ip_address, '222.222.222.222')
