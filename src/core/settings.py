f66caf407b4196848259e6af93d976e38ad8a0fb
JonathasSC <jonathas.s.cardoso28@gmail.com>
2025-10-19 21:56:24 -0300

    feat: add user_balance context

--- src/core/settings.py
+++ src/core/settings.py
@@ -149,6 +149,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.billing.context_processors.user_balance',
             ],
         },
     },
