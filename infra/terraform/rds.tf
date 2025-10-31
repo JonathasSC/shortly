f5d16400ae637faf92a6c4219cef7ddcb0f05ded
JonathasSC <jonathas.s.cardoso28@gmail.com>
2025-10-31 16:43:58 -0300

    refator add terraform

--- infra/terraform/rds.tf
+++ infra/terraform/rds.tf
@@ -8,11 +8,11 @@ resource "aws_db_instance" "postgres" {
   identifier              = "shortly-db"
   engine                  = "postgres"
   engine_version          = "15"
-  instance_class          = "db.t3.micro"
+  instance_class          = var.instance_class
   allocated_storage       = 20
   storage_type            = "gp2"
-  username = var.db_username
-  password = var.db_password
+  username                = var.db_username
+  password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   skip_final_snapshot     = true
