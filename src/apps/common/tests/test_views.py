fa0a833e9c22e1b250388bb82ed41460d50cc0d4
JonathasSC <jonathas.s.cardoso28@gmail.com>
2025-11-09 21:27:20 -0300

    test(common): update assetEqual status code

--- src/apps/common/tests/test_views.py
+++ src/apps/common/tests/test_views.py
@@ -6,5 +6,5 @@ class NotFoundViewTest(TestCase):
     def test_notfound_view_status_code_and_template(self):
         url = reverse('notfound')
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
         self.assertTemplateUsed(response, 'common/404.html')
