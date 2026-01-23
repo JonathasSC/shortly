d5d522c2c5c118cbecb1dd8f37ef31c3abf1deb2
Jonathas Cardoso <jonathas.s.cardoso28@gmail.com>
2026-01-22 22:30:40 -0300

    refactor: add database django settings .env callbacks

--- src/core/settings.py
+++ src/core/settings.py
@@ -171,12 +171,12 @@ TEMPLATES = [
 
 DATABASES = {
     "default": {
-        "ENGINE": f"django.db.backends.{os.getenv('DB_ENGINE')}",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "ENGINE": f"django.db.backends.{os.getenv('DB_ENGINE', 'sqlite3')}",
+        "NAME": os.getenv("DB_NAME", ":memory:"),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", ""),
     }
 }
 
