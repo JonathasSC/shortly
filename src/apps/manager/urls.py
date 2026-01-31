92dda6a5761fde38d0f98fdfde1f9beefdc3edb1
Jonathas Cardoso <jonathas.s.cardoso28@gmail.com>
2026-01-30 22:07:12 -0300

    refactor: create get_websocket_urlspatterns

--- src/apps/manager/urls.py
+++ src/apps/manager/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, re_path
 
-from apps.manager.consumers import LogsConsumer
-
 from .views import DashboardView
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
 
-websocket_urlpatterns = [
-    re_path(r"ws/logs/$", LogsConsumer.as_asgi()),
-]
+
+def get_websocket_urlpatterns():
+    from apps.manager.consumers import LogsConsumer
+    return [
+        re_path("ws/logs/", LogsConsumer.as_asgi()),
+    ]
--- src/core/asgi.py
+++ src/core/asgi.py
@@ -1,19 +1,20 @@
 import os
 
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from apps.manager.urls import websocket_urlpatterns
+from apps.manager.urls import get_websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
 
 django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(get_websocket_urlpatterns())
     ),
 })
