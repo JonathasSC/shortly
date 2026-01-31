8308e4d74c70ee7c6a532d5787f905863ea0a5ad
Jonathas Cardoso <jonathas.s.cardoso28@gmail.com>
2026-01-30 22:16:19 -0300

    revert: create get_websocket_urlspatterns

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -1,20 +1,19 @@
+from apps.manager.urls import websocket_urlpatterns
 import os
-
 import django
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
-from apps.manager.urls import get_websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django.setup()
+
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(get_websocket_urlpatterns())
+        URLRouter(websocket_urlpatterns)
     ),
 })
