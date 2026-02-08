cb1b63e09d7f313fc2e6850335251023e85f2763
Jonathas Cardoso <jonathas.s.cardoso28@gmail.com>
2026-02-08 20:30:04 -0300

    refactor: run linter

--- src/apps/billing/webhook.py
+++ src/apps/billing/webhook.py
@@ -2,6 +2,7 @@ import hashlib
 import hmac
 import json
 import logging
+from dataclasses import asdict
 
 import mercadopago
 from django.conf import settings
@@ -11,7 +12,6 @@ from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from dataclasses import asdict
 
 from apps.billing.dto import PaymentDataDTO
 from apps.billing.models import UserWallet, WalletTransaction
