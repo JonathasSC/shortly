25709c01b25e3a386e1625399ff94f82df024830
Jonathas Cardoso <jonathas.s.cardoso28@gmail.com>
2026-02-27 00:40:43 -0300

    feat: update admin page for urls

--- src/apps/converter/admin.py
+++ src/apps/converter/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
-from .models import Url, UrlMetadata, AccessEvent, UrlSequence
+
+from .models import AccessEvent, Url, UrlMetadata, UrlSequence
+
 
 class UrlMetadataInline(admin.StackedInline):
     model = UrlMetadata
